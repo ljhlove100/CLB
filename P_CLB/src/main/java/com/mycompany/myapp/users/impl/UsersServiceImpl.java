c38f5fe3db20e83af8b37459bd9ecb16e0935ec8
JeongJunWoo <35982722+rpdladyd11@users.noreply.github.com>
2018-07-02 18:00:58 +0900

    ok

--- P_CLB/src/main/java/com/mycompany/myapp/users/impl/UsersServiceImpl.java
+++ P_CLB/src/main/java/com/mycompany/myapp/users/impl/UsersServiceImpl.java
@@ -15,82 +15,82 @@ import com.mycompany.myapp.users.UsersVO;
 @Service("usersService")
 public class UsersServiceImpl implements UsersService {
 
-   @Autowired
-   UsersDAO dao;
+	@Autowired
+	UsersDAO dao;
 
-   @Override
-   public int getLoginck(String u_id, String pw) {
-      // TODO Auto-generated method stub
-      HashMap<String,String> hashMap = new HashMap<String,String>();
-       hashMap.put("u_id", u_id);
-       hashMap.put("pw", pw);
-       
-       return dao.getLoginck(hashMap);
+	@Override
+	public int getLoginck(String u_id, String pw) {
+		// TODO Auto-generated method stub
+		HashMap<String,String> hashMap = new HashMap<String,String>();
+    	hashMap.put("u_id", u_id);
+    	hashMap.put("pw", pw);
+    	
+    	return dao.getLoginck(hashMap);
     }
 
-   @Override
-   public List<Map<String, Object>> getUserList() {
-      // TODO Auto-generated method stub
-      return dao.getUsersList();
-   }
+	@Override
+	public List<Map<String, Object>> getUserList() {
+		// TODO Auto-generated method stub
+		return dao.getUsersList();
+	}
 
-   @Override
-   public List<UsersVO> getUserList(UsersVO vo) {
-      // TODO Auto-generated method stub
-      return dao.getUsersList(vo);
-   }
+	@Override
+	public List<UsersVO> getUserList(UsersVO vo) {
+		// TODO Auto-generated method stub
+		return dao.getUsersList(vo);
+	}
 
-   @Override
-   public UsersVO getUsers(String u_id) {
-      // TODO Auto-generated method stub
-      return dao.getUsers(u_id);
-   }
+	@Override
+	public UsersVO getUsers(String u_id) {
+		// TODO Auto-generated method stub
+		return dao.getUsers(u_id);
+	}
 
-   @Override
-   public void insertUsers(UsersVO vo) {
-      // TODO Auto-generated method stub
-      dao.insertUsers(vo);
-   }
+	@Override
+	public void insertUsers(UsersVO vo) {
+		// TODO Auto-generated method stub
+		dao.insertUsers(vo);
+	}
 
-   @Override
-   public void updateUsers(UsersVO vo) {
-      // TODO Auto-generated method stub
-      dao.updateUsers(vo);
-   }
+	@Override
+	public void updateUsers(UsersVO vo) {
+		// TODO Auto-generated method stub
+		dao.updateUsers(vo);
+	}
 
-   @Override
-   public void deleteUsers(String u_id) {
-      // TODO Auto-generated method stub
-      dao.deleteUsers(u_id);
-   }
+	@Override
+	public void deleteUsers(String u_id) {
+		// TODO Auto-generated method stub
+		dao.deleteUsers(u_id);
+	}
 
-   @Override
-   public void Login(String Login) {
-      // TODO Auto-generated method stub
-      dao.Login(Login);
-   }
+	@Override
+	public void Login(String Login) {
+		// TODO Auto-generated method stub
+		dao.Login(Login);
+	}
 
-   @Override
-   public String getUsersName(String u_id) {
-      // TODO Auto-generated method stub
+	@Override
+	public String getUsersName(String u_id) {
+		// TODO Auto-generated method stub
 
-      HashMap<String,String> hashMap = new HashMap<String,String>();
-       hashMap.put("u_id", u_id);       
-       
-       return dao.getUsersName(hashMap);
+		HashMap<String,String> hashMap = new HashMap<String,String>();
+    	hashMap.put("u_id", u_id);    	
+    	
+    	return dao.getUsersName(hashMap);
     }
-   
-   @Override
-   public void account(String u_id, String pw, String name, String yn) {
-      // TODO Auto-generated method stub
+	
+	@Override
+	public void account(String u_id, String pw, String name, String yn) {
+		// TODO Auto-generated method stub
 
-      HashMap<String,String> hashMap = new HashMap<String,String>();
-       hashMap.put("u_id", u_id);  
-       hashMap.put("pw", pw);
-       hashMap.put("name", name);
-       hashMap.put("yn", yn);
-       
-       dao.account(hashMap);
+		HashMap<String,String> hashMap = new HashMap<String,String>();
+    	hashMap.put("u_id", u_id);  
+    	hashMap.put("pw", pw);
+    	hashMap.put("name", name);
+    	hashMap.put("yn", yn);
+    	
+    	dao.account(hashMap);
     }
 
 	//관리자권한
--- P_CLB/src/main/java/com/mycompany/myapp/users/view/UsersController.java
+++ P_CLB/src/main/java/com/mycompany/myapp/users/view/UsersController.java
@@ -34,117 +34,75 @@ import com.mycompany.myapp.users.impl.UsersDAO;
 @SessionAttributes("users")
 public class UsersController {
 
-   
-   
-   @Autowired
-   UsersService UsersService;
+	
+	
+	@Autowired
+	UsersService UsersService;
 
-   @RequestMapping(value = "/index", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(){
-                 
-       return "clb/index";
+    	    		
+    	return "clb/index";
     }
-   
-   
-   @RequestMapping(value = "/home", method = RequestMethod.GET)
+	
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(){
-                 
-       return "users/login";
+    	    		
+    	return "users/login";
     }
-   
-      
-   @RequestMapping(value = "/LogOut", method = RequestMethod.GET)
-    public String Logout( HttpSession session){       
+	
+		
+	@RequestMapping(value = "/LogOut", method = RequestMethod.GET)
+    public String Logout( HttpSession session){    	
 
-      session.invalidate();
-      
-       return "redirect:/";
+		session.invalidate();
+		
+    	return "redirect:/";
     }
-   
-   @RequestMapping(value = "/login", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
     public String Login(){
-                 
-       return "users/login";
-    }   
-   
-   @RequestMapping("/getUsersList")
-   public String getUsersList(UsersSearchVO vo, HttpServletRequest request) {
-      //request.setAttribute("usersList", UsersService.getUserList());
-      return "users/getUsersList";
-   }
+    	    		
+    	return "users/login";
+    }	
+	
+	@RequestMapping("/getUsersList")
+	public String getUsersList(UsersSearchVO vo, HttpServletRequest request) {
+		//request.setAttribute("usersList", UsersService.getUserList());
+		return "users/getUsersList";
+	}
 
-   // 등록폼
-   @RequestMapping(value = "/usersInsert", method = RequestMethod.GET)
-   public String usersInsertForm() {
-      return "users/getUsersInsert";
-   }
+	// 등록폼
+	@RequestMapping(value = "/usersInsert", method = RequestMethod.GET)
+	public String usersInsertForm() {
+		return "users/getUsersInsert";
+	}
 
-   // 수정폼
-   @RequestMapping(value = "/UsersUpdate", method = RequestMethod.GET)
-   public String usersUpdateForm(Model model, UsersVO vo) {
-      model.addAttribute("users",UsersService.getUsers(vo.getuId()));
-      return "users/getUsersupdate";
-   }
+	// 수정폼
+	@RequestMapping(value = "/UsersUpdate", method = RequestMethod.GET)
+	public String usersUpdateForm(Model model, UsersVO vo) {
+		model.addAttribute("users",UsersService.getUsers(vo.getuId()));
+		return "users/getUsersupdate";
+	}
 
-   // 수정처리
-   @RequestMapping(value = "/UsersUpdate", method = RequestMethod.POST)
-   public String updateUsers(@ModelAttribute("users") UsersVO vo) {
-      System.out.println(vo);
-      UsersService.updateUsers(vo);
-      return "redirect:/getUsersList";
-   }
+	// 수정처리
+	@RequestMapping(value = "/UsersUpdate", method = RequestMethod.POST)
+	public String updateUsers(@ModelAttribute("users") UsersVO vo) {
+		System.out.println(vo);
+		UsersService.updateUsers(vo);
+		return "redirect:/getUsersList";
+	}
 
-   // 등록처리
-   @RequestMapping(value = "/usersInsert", method = RequestMethod.POST)
-   public String insertBoard(@ModelAttribute("users") UsersVO vo) {
-      System.out.println(vo);
-      // 서비스이용하여 등록처리하고 목록페이지로 이동
-      UsersService.insertUsers(vo);
-      return "users/UsersInsert";
-   }
+	// 등록처리
+	@RequestMapping(value = "/usersInsert", method = RequestMethod.POST)
+	public String insertBoard(@ModelAttribute("users") UsersVO vo) {
+		System.out.println(vo);
+		// 서비스이용하여 등록처리하고 목록페이지로 이동
+		UsersService.insertUsers(vo);
+		return "users/UsersInsert";
+	}
 
-<<<<<<< HEAD
-   @RequestMapping("/getUsers/{u_id}")
-   public String getUsers(@PathVariable String u_id, Model model) {
-      model.addAttribute("users", UsersService.getUsers(u_id));
-      return "users/getUsers";
-   }
-   
-   // 로그인
-   @RequestMapping(value = "/loginck", method = RequestMethod.POST)
-   public String loginck(Model model,  HttpServletRequest request) {
-      
-      String   u_id = request.getParameter("u_id");
-      String   pw = request.getParameter("pw");
-      String    ReturnUrl = "";
-      
-      int loginck =  UsersService.getLoginck(u_id,pw);
-      
-      //users 정보가 있다면 1 없으면 0
-      if(loginck==1){
-         
-         Map<String, Object> map = new HashMap<String,Object>();
-         
-         String name  = UsersService.getUsersName(u_id);
-                  
-         
-         request.getSession().setAttribute("u_id", u_id);
-         
-         ReturnUrl = "redirect:getProductList";
-      
-      } else {
-         
-         model.addAttribute("message", "ID와PW를 확인해주세요");
-         ReturnUrl = "login";
-         
-      } return ReturnUrl;
-   }
-   
-   //회원가입
-      @RequestMapping(value = "/account", method = RequestMethod.GET)
-       public String account(Model model,  HttpServletRequest request,
-             @RequestParam(required=false, defaultValue="1") int cnt ) {
-=======
 	@RequestMapping("/getUsers/{u_id}")
 	public String getUsers(@PathVariable String u_id, Model model) {
 		model.addAttribute("users", UsersService.getUsers(u_id));
@@ -193,43 +151,7 @@ public class UsersController {
 		@RequestMapping(value = "/account", method = RequestMethod.GET)
 	    public String account(Model model,  HttpServletRequest request,
 	    		@RequestParam(required=false, defaultValue="1") int cnt ) {
->>>>>>> branch 'master' of https://github.com/ljhlove100/CLB
 
-<<<<<<< HEAD
-         String Url = "";
-      
-         if(cnt == 1)
-         {
-            Url="users/account";
-         }
-         else {
-            String   u_id = request.getParameter("u_id");
-            String   pw = request.getParameter("pw");
-            String   name = request.getParameter("name");
-            String    yn = "Y";
-            System.out.println(u_id + pw + name+yn);
-            UsersService.account(u_id, pw, name, yn);
-            
-            Url="redirect:/login";
-         }
-                    
-          return Url;
-       }
-      
-   //ID 중복체크
-      /*@ResponseBody
-      @RequestMapping(value = "/checkId", method = RequestMethod.POST)
-      public String checkId(Model model,  HttpServletRequest request) {
-         System.out.println("Controller.idCheck() 호출");
-           int result=0;
-           UsersDAO user=UsersService.getUser(vo);
-           if(user!=null) result=1;
-           else System.out.println("아이디사용가능");
-           return result;
-      }*/
-   
-      
-=======
 			String Url = "";
 		
 			if(cnt == 1)
@@ -263,5 +185,4 @@ public class UsersController {
 		}*/
 	
 		
->>>>>>> branch 'master' of https://github.com/ljhlove100/CLB
 }
